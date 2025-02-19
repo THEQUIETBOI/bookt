cda95710ff49a429513cabcc2dfb256496586063
[THEQUIETBOI] <[gmedina0518@gmail.com]>
2025-02-18 23:43:36 -0500

    update

--- bookmark.js
+++ bookmark.js
@@ -50,7 +50,11 @@ const menuContainer = document.createElement('div');
 
   const add = document.createElement('button');
   add.id = 'add';
-  add.innerHTML = "+";
+  add.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" version="1.1" id="Capa_1" width="20px" height="20px" viewBox="0 0 45.402 45.402" xml:space="preserve">
+<g>
+	<path d="M41.267,18.557H26.832V4.134C26.832,1.851,24.99,0,22.707,0c-2.283,0-4.124,1.851-4.124,4.135v14.432H4.141   c-2.283,0-4.139,1.851-4.138,4.135c-0.001,1.141,0.46,2.187,1.207,2.934c0.748,0.749,1.78,1.222,2.92,1.222h14.453V41.27   c0,1.142,0.453,2.176,1.201,2.922c0.748,0.748,1.777,1.211,2.919,1.211c2.282,0,4.129-1.851,4.129-4.133V26.857h14.435   c2.283,0,4.134-1.867,4.133-4.15C45.399,20.425,43.548,18.557,41.267,18.557z"/>
+</g>
+</svg>`;
   menuContainerButtons.appendChild(add);
 
   const ChatInput = document.createElement('input');
@@ -65,13 +69,23 @@ const menuContainer = document.createElement('div');
 
   const settingsButton = document.createElement('button');
   settingsButton.className ='settings';
-settingsButton.innerHTML = "⚙️";
+settingsButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="40px" width="40px" version="1.1" id="Capa_1" viewBox="0 0 489.802 489.802" xml:space="preserve">
+<g>
+	<path d="M20.701,281.901l32.1,0.2c4.8,24.7,14.3,48.7,28.7,70.5l-22.8,22.6c-8.2,8.1-8.2,21.2-0.2,29.4l24.6,24.9   c8.1,8.2,21.2,8.2,29.4,0.2l22.8-22.6c21.6,14.6,45.5,24.5,70.2,29.5l-0.2,32.1c-0.1,11.5,9.2,20.8,20.7,20.9l35,0.2   c11.5,0.1,20.8-9.2,20.9-20.7l0.2-32.1c24.7-4.8,48.7-14.3,70.5-28.7l22.6,22.8c8.1,8.2,21.2,8.2,29.4,0.2l24.9-24.6   c8.2-8.1,8.2-21.2,0.2-29.4l-22.6-22.8c14.6-21.6,24.5-45.5,29.5-70.2l32.1,0.2c11.5,0.1,20.8-9.2,20.9-20.7l0.2-35   c0.1-11.5-9.2-20.8-20.7-20.9l-32.1-0.2c-4.8-24.7-14.3-48.7-28.7-70.5l22.8-22.6c8.2-8.1,8.2-21.2,0.2-29.4l-24.6-24.9   c-8.1-8.2-21.2-8.2-29.4-0.2l-22.8,22.6c-21.6-14.6-45.5-24.5-70.2-29.5l0.2-32.1c0.1-11.5-9.2-20.8-20.7-20.9l-35-0.2   c-11.5-0.1-20.8,9.2-20.9,20.7l-0.3,32.1c-24.8,4.8-48.8,14.3-70.5,28.7l-22.6-22.8c-8.1-8.2-21.2-8.2-29.4-0.2l-24.8,24.6   c-8.2,8.1-8.2,21.2-0.2,29.4l22.6,22.8c-14.6,21.6-24.5,45.5-29.5,70.2l-32.1-0.2c-11.5-0.1-20.8,9.2-20.9,20.7l-0.2,35   C-0.099,272.401,9.201,281.801,20.701,281.901z M179.301,178.601c36.6-36.2,95.5-35.9,131.7,0.7s35.9,95.5-0.7,131.7   s-95.5,35.9-131.7-0.7S142.701,214.801,179.301,178.601z"/>
+</g>
+</svg>`;
   menuBar.appendChild(settingsButton);
 
 
   const settingsLabel = document.createElement('label');
   settingsLabel.className = 'custom-file-upload';
   settingsLabel.for = 'hide';
+  settingsLabel.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="40px" width="40px" version="1.1" id="Capa_1" viewBox="0 0 374.116 374.116" xml:space="preserve">
+<g>
+	<path d="M344.058,207.506c-16.568,0-30,13.432-30,30v76.609h-254v-76.609c0-16.568-13.432-30-30-30c-16.568,0-30,13.432-30,30   v106.609c0,16.568,13.432,30,30,30h314c16.568,0,30-13.432,30-30V237.506C374.058,220.938,360.626,207.506,344.058,207.506z"/>
+	<path d="M123.57,135.915l33.488-33.488v111.775c0,16.568,13.432,30,30,30c16.568,0,30-13.432,30-30V102.426l33.488,33.488   c5.857,5.858,13.535,8.787,21.213,8.787c7.678,0,15.355-2.929,21.213-8.787c11.716-11.716,11.716-30.71,0-42.426L208.271,8.788   c-11.715-11.717-30.711-11.717-42.426,0L81.144,93.489c-11.716,11.716-11.716,30.71,0,42.426   C92.859,147.631,111.855,147.631,123.57,135.915z"/>
+</g>
+</svg>`
   menuBar.appendChild(settingsLabel);
 
 
@@ -93,15 +107,47 @@ const stylesheet = document.createElement('style');
   document.head.appendChild(marcopolo);
  
 
-  
   settingsButton.addEventListener('click', () => {
     settingsPage.style.visibility = "visible";
-  });
+});
 
-  close.addEventListener('click', () => {
+close.addEventListener('click', () => {
     settingsPage.style.visibility = "hidden";
-  });
+});
 
+
+// Mobile-Friendly Approach (using both click and touch)
+settingsButton.addEventListener('click', showSettings);
+settingsButton.addEventListener('touchstart', showSettings); // For touch devices
+
+close.addEventListener('click', hideSettings);
+close.addEventListener('touchstart', hideSettings); // For touch devices
+
+
+function showSettings(e) {
+    e.preventDefault(); // Prevent default touch behavior (like scrolling) if needed
+    settingsPage.style.visibility = "visible";
+}
+
+function hideSettings(e) {
+    e.preventDefault(); // Prevent default touch behavior if needed
+    settingsPage.style.visibility = "hidden";
+}
+
+
+// Another approach using pointer events (more modern, but might need polyfills for older browsers):
+settingsButton.addEventListener('pointerdown', showSettings);
+close.addEventListener('pointerdown', hideSettings);
+
+function showSettings(e) {
+    e.preventDefault(); // Prevent default touch behavior (like scrolling) if needed
+    settingsPage.style.visibility = "visible";
+}
+
+function hideSettings(e) {
+    e.preventDefault(); // Prevent default touch behavior if needed
+    settingsPage.style.visibility = "hidden";
+}
 
 
 
@@ -209,13 +255,13 @@ const stylesheet = document.createElement('style');
 
 .me-container #add {
     border-radius: 100%; 
-    background-image: url("https://www.svgrepo.com/show/2087/plus.svg");
     background-color: transparent; 
     background-position: center; 
     background-repeat: no-repeat; 
     background-size: cover;
     border: none; 
     padding:10px; 
+    font-size 30px;
     cursor: pointer; 
     user-select: none;
 }
@@ -261,15 +307,14 @@ const stylesheet = document.createElement('style');
 }
 
 .settings{
-background-image: url("https://www.svgrepo.com/show/10207/settings.svg");
 background-position: center;
 background-repeat: no-repeat;
 background-size: contain;
     position: relative;
     display: flex;
     padding: 10px;
-    width: 30px;
-    height: 30px;
+    width: 50px;
+    height: 50px;
     border-color: transparent;
     background-color: transparent;
 }
@@ -312,7 +357,6 @@ visibility:hidden;
 
 .custom-file-upload {
    
-background-image: url("https://www.svgrepo.com/show/28557/upload-sign.svg");
 background-position: center;
 background-repeat: no-repeat;
 background-size: contain;
