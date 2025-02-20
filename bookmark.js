70de9aab9657a64749b5ec64d810af238926c78f
[THEQUIETBOI] <[gmedina0518@gmail.com]>
2025-02-19 22:29:28 -0500

    update

--- bookmark.js
+++ bookmark.js
@@ -48,24 +48,6 @@ const menuContainer = document.createElement('div');
   menuContainerButtons.className ='menu-container-buttons';
   menuContainer.appendChild(menuContainerButtons);
 
-  const add = document.createElement('button');
-  add.id = 'add';
-  add.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" version="1.1" id="Capa_1" width="20px" height="20px" viewBox="0 0 45.402 45.402" xml:space="preserve">
-<g>
-	<path d="M41.267,18.557H26.832V4.134C26.832,1.851,24.99,0,22.707,0c-2.283,0-4.124,1.851-4.124,4.135v14.432H4.141   c-2.283,0-4.139,1.851-4.138,4.135c-0.001,1.141,0.46,2.187,1.207,2.934c0.748,0.749,1.78,1.222,2.92,1.222h14.453V41.27   c0,1.142,0.453,2.176,1.201,2.922c0.748,0.748,1.777,1.211,2.919,1.211c2.282,0,4.129-1.851,4.129-4.133V26.857h14.435   c2.283,0,4.134-1.867,4.133-4.15C45.399,20.425,43.548,18.557,41.267,18.557z"/>
-</g>
-</svg>`;
-  menuContainerButtons.appendChild(add);
-
-  const ChatInput = document.createElement('input');
-  ChatInput.type = "text";
-  ChatInput.className = "chat-input";
-  ChatInput.placeholder = "Type here...";
-  menuContainerButtons.appendChild(ChatInput);
-
-  const menuBar = document.createElement('div');
-  menuBar.className = 'menu-bar';
-  menuContainerButtons.appendChild(menuBar);
 
   const settingsButton = document.createElement('button');
   settingsButton.className ='settings';
@@ -74,25 +56,23 @@ settingsButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink=
 	<path d="M20.701,281.901l32.1,0.2c4.8,24.7,14.3,48.7,28.7,70.5l-22.8,22.6c-8.2,8.1-8.2,21.2-0.2,29.4l24.6,24.9   c8.1,8.2,21.2,8.2,29.4,0.2l22.8-22.6c21.6,14.6,45.5,24.5,70.2,29.5l-0.2,32.1c-0.1,11.5,9.2,20.8,20.7,20.9l35,0.2   c11.5,0.1,20.8-9.2,20.9-20.7l0.2-32.1c24.7-4.8,48.7-14.3,70.5-28.7l22.6,22.8c8.1,8.2,21.2,8.2,29.4,0.2l24.9-24.6   c8.2-8.1,8.2-21.2,0.2-29.4l-22.6-22.8c14.6-21.6,24.5-45.5,29.5-70.2l32.1,0.2c11.5,0.1,20.8-9.2,20.9-20.7l0.2-35   c0.1-11.5-9.2-20.8-20.7-20.9l-32.1-0.2c-4.8-24.7-14.3-48.7-28.7-70.5l22.8-22.6c8.2-8.1,8.2-21.2,0.2-29.4l-24.6-24.9   c-8.1-8.2-21.2-8.2-29.4-0.2l-22.8,22.6c-21.6-14.6-45.5-24.5-70.2-29.5l0.2-32.1c0.1-11.5-9.2-20.8-20.7-20.9l-35-0.2   c-11.5-0.1-20.8,9.2-20.9,20.7l-0.3,32.1c-24.8,4.8-48.8,14.3-70.5,28.7l-22.6-22.8c-8.1-8.2-21.2-8.2-29.4-0.2l-24.8,24.6   c-8.2,8.1-8.2,21.2-0.2,29.4l22.6,22.8c-14.6,21.6-24.5,45.5-29.5,70.2l-32.1-0.2c-11.5-0.1-20.8,9.2-20.9,20.7l-0.2,35   C-0.099,272.401,9.201,281.801,20.701,281.901z M179.301,178.601c36.6-36.2,95.5-35.9,131.7,0.7s35.9,95.5-0.7,131.7   s-95.5,35.9-131.7-0.7S142.701,214.801,179.301,178.601z"/>
 </g>
 </svg>`;
-  menuBar.appendChild(settingsButton);
+  menuContainerButtons.appendChild(settingsButton);
+
+  const ChatInput = document.createElement('input');
+  ChatInput.type = "text";
+  ChatInput.className = "chat-input";
+  ChatInput.placeholder = "Type here...";
+  menuContainerButtons.appendChild(ChatInput);
 
 
-  const settingsLabel = document.createElement('label');
-  settingsLabel.className = 'custom-file-upload';
-  settingsLabel.for = 'hide';
-  settingsLabel.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="40px" width="40px" version="1.1" id="Capa_1" viewBox="0 0 374.116 374.116" xml:space="preserve">
-<g>
-	<path d="M344.058,207.506c-16.568,0-30,13.432-30,30v76.609h-254v-76.609c0-16.568-13.432-30-30-30c-16.568,0-30,13.432-30,30   v106.609c0,16.568,13.432,30,30,30h314c16.568,0,30-13.432,30-30V237.506C374.058,220.938,360.626,207.506,344.058,207.506z"/>
-	<path d="M123.57,135.915l33.488-33.488v111.775c0,16.568,13.432,30,30,30c16.568,0,30-13.432,30-30V102.426l33.488,33.488   c5.857,5.858,13.535,8.787,21.213,8.787c7.678,0,15.355-2.929,21.213-8.787c11.716-11.716,11.716-30.71,0-42.426L208.271,8.788   c-11.715-11.717-30.711-11.717-42.426,0L81.144,93.489c-11.716,11.716-11.716,30.71,0,42.426   C92.859,147.631,111.855,147.631,123.57,135.915z"/>
-</g>
-</svg>`
-  menuBar.appendChild(settingsLabel);
+
+  
 
 
-  const fileUpload = document.createElement('input');
-  fileUpload.type = 'file';
-  fileUpload.id = 'hide';
-  settingsLabel.appendChild(fileUpload);
+  
+
+
+
 
 
 const stylesheet = document.createElement('style');
@@ -151,34 +131,34 @@ close.onclick = () => {
 
 .chat-container {
     background-color:#3d425c;
-    height: calc(100% - 90px); 
+    height: calc(100% - 70px); 
     width: 100%; 
     overflow-y: auto;
     overflow-x: hidden;
     box-sizing: border-box; 
     padding: 10px;
+    margin-top: 25px;
 }
 
 
-.botmessage {
+.botMessage  {
     color: rgb(255, 255, 255);
     font-family: 13px "monospace"; 
     font-weight: 600;
     background-color: rgb(32, 32, 32);
     border-radius: 10px;
-    margin-top: 30px;
-    padding: 30px;
     overflow-wrap: break-word; 
-}
+    margin-top: 10px;
+
+    }
 
 
-.usermessage {
+.userMessage {
     color: rgb(255, 255, 255);
     font-weight: 600;
     background-color: rgb(32, 32, 32);
     border-radius: 10px;
- margin-top: 30px;
- padding: 30px;
+    margin-top: 10px;
 
 }
 .me-container {
@@ -199,8 +179,8 @@ close.onclick = () => {
     display:flex;
     justify-content:center;
     align-items: center;
-    gap: 10px;
-    padding: 10px;
+    gap: 5px;
+    padding: 5px;
     border-radius: 50px;
     border: 4px solid tan;
 
@@ -216,62 +196,12 @@ close.onclick = () => {
     border-radius: 10px;
 }
 
-.me-container #add {
-    border-radius: 100%; 
-    background-color: transparent; 
-    background-position: center; 
-    background-repeat: no-repeat; 
-    background-size: cover;
-    border: none; 
-    padding:10px; 
-    font-size 30px;
-    cursor: pointer; 
-    user-select: none;
-     -webkit-appearance: none; /* Remove default button styling (iOS) */
-    -moz-appearance: none; /* Remove default button styling (Firefox) */
-    appearance: none; /* Remove default button styling (general) */
-    touch-action: manipulation; /* Improves touch responsiveness */
-}
 
 
 
 
 
-.menu-bar{
-    height: 100px;
-    width: 40px;
-    border-radius: 30px;
-    position: absolute;
-    justify-items: center;
-    align-content: center;
-    opacity: 0%;
-    left: 0;
-    bottom: 70px;
-    background-color: gray;
-    z-index: 1000;
 
-
-
-}
-
-.menu-bar button {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-}
-
-#add:focus ~ .menu-bar {
-    height: 100px;
-    display: block;
-    width: 40px;
-    border-radius: 30px;
-    position: absolute;
-    opacity: 100%;
-    bottom: 70px;
-    background-color: gray;
-
-}
 
 .settings{
 background-position: center;
@@ -279,9 +209,7 @@ background-repeat: no-repeat;
 background-size: contain;
     position: relative;
     display: flex;
-    padding: 10px;
-    width: 50px;
-    height: 50px;
+    width: 40px;
     border-color: transparent;
     background-color: transparent;
 }
@@ -352,20 +280,19 @@ padding: 10px;
 font-family: 'Times New Roman', Times, serif;
 
 }
-p{ 
-    overflow: auto;
-    padding: 30px;
-    overflow-wrap: break-word;
-}
+
 
 .hidden {
     display: none;
 }
-    @media only screen and (max-width: 600px) {
-    .main-container {
-        /* ... existing styles ... */
-        font-size: 14px; /* Adjust as needed */
-    }
+  
+
+
+
+
+
+
+    
 }
 
   `;
